a7b029336965014be1958c91360723944dd51215
Max Keybr <maximefim@gmail.com>
2020-10-22 18:41:28 +0700

    feat: solution brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const openBr = bracketsConfig.map((i) => i[0]);
+    const closeBr = bracketsConfig.map((i) => i[1]);
+    const newArr = [];
+
+    for (let i = 0; i < str.length; i += 1) {
+        const openIndex = openBr.indexOf(str[i]);
+        const closeIndex = closeBr.indexOf(str[i]);
+
+        if (closeIndex === openIndex) {
+            if (newArr.length > 0 && newArr[newArr.length - 1] === closeIndex) {
+                newArr.pop();
+            } else {
+                newArr.push(openIndex);
+            }
+        } else if (openIndex !== -1) {
+            newArr.push(openIndex);
+        } else {
+            if (newArr[newArr.length - 1] === closeIndex) {
+                newArr.pop();
+            } else return false;
+        }
+    }
+
+    return newArr.length === 0;
+};
